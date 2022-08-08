9516ae38c98c6d036799a964b881def85dd5ed99
Ezequiel Ramos <ezequielmr94@gmail.com>
2022-08-08 17:46:33 -0300

    using navigation prop

--- src/pages/CameraList.tsx
+++ src/pages/CameraList.tsx
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function CameraList() {
+
+interface CameraListProps {
+    navigation: Navigation;
+}
+
+export default function CameraList({ navigation }: CameraListProps) {
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>

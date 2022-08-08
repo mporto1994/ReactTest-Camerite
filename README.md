9618b9e291c830f4193afeac7f500152cf0f98d0
Ezequiel Ramos <ezequielmr94@gmail.com>
2022-08-08 16:34:07 -0300

    adding backend and initial dependencies

---
+++ README.md
@@ -0,0 +1,32 @@
+# ReactTest
+
+## Install
+
+### How to Install?
+
+```bash
+nvm use
+npm install
+```
+
+### Expo-Cli
+
+If it the first time that you are running the application, you need to install expo-cli globally. You can see more [Here](https://docs.expo.dev).
+
+```bash
+nvm use
+npm install --global expo-cli
+```
+
+### How to run?
+
+```bash
+nvm use
+npm run web
+```
+
+#### Known Issues
+
+If you get the error `ENOSPC: System limit for number of file watchers reached` on running front-end-pwa application and you're running Linux, get a look on:
+
+<https://stackoverflow.com/questions/55763428/react-native-error-enospc-system-limit-for-number-of-file-watchers-reached>
---
+++ backend/app.ts
@@ -0,0 +1,188 @@
+import express, { Express, json, Request, Response } from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+interface CameraParam {
+    title: string;
+    plan: '1dia' | '3dias' | '7dias';
+    external: boolean;
+}
+
+interface Camera extends CameraParam {
+    id: number;
+    image_url: string;
+}
+let autoIncrementCameraId = 0;
+
+const cameras: { [cameraId: string]: Camera; } = {};
+
+interface WebService {
+    port: string;
+    app: Express;
+}
+
+class ExtendableError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+        if (typeof Error.captureStackTrace === 'function') {
+            Error.captureStackTrace(this, this.constructor);
+        } else {
+            this.stack = (new Error(message)).stack;
+        }
+    }
+}
+
+class InvalidTitleValue extends ExtendableError {
+}
+
+class InvalidPlanValue extends ExtendableError {
+}
+
+class InvalidExternalValue extends ExtendableError {
+}
+
+class WebService {
+    constructor() {
+        this.port = process.env.PORT || "5000";
+        this.app = express();
+
+        this.app.use(bodyParser.json({
+            limit: '10mb'
+        }));
+
+        this.app.use(morgan("common"));
+
+        console.log("[WebService] - Adding routes");
+
+        this.defineRoutes();
+    }
+
+    addCamera(cameraParam: CameraParam): Camera {
+        autoIncrementCameraId += 1;
+        const camera: Camera = {
+            id: autoIncrementCameraId,
+            title: cameraParam.title,
+            plan: cameraParam.plan,
+            external: cameraParam.external,
+            image_url: 'http://localhost:5000/placeholder'
+        };
+
+        cameras[autoIncrementCameraId.toString()] = camera;
+
+        return camera;
+    }
+
+    validRequest(cameraParam: CameraParam): CameraParam {
+
+        if (typeof cameraParam.title != 'string') {
+            throw new InvalidTitleValue('Invalid value for `title` parameter. Use string instead.');
+        }
+
+        if (typeof cameraParam.external != 'boolean') {
+            throw new InvalidExternalValue('Invalid value for `external` parameter. Use boolean (true, false) instead.');
+        }
+
+        if (typeof cameraParam.plan != 'string' || (!['1dia', '3dias', '7dias'].includes(cameraParam.plan))) {
+            throw new InvalidPlanValue('Invalid value for `plan` parameter. Use `1dia`|`3dias`|`7dias` instead.');
+        }
+
+        return cameraParam;
+    }
+
+    defineRoutes(): void {
+        this.app.get('/', (req: Request, res: Response) => {
+            res.status(200).send('OK');
+        });
+
+        this.app.get('/cameras', (req: Request, res: Response) => {
+            const cameraList: Camera[] = [];
+            for (const cameraId in cameras) {
+                cameraList.push(cameras[cameraId]);
+            }
+            res.json(cameraList);
+        });
+
+        this.app.post('/cameras', (req: Request, res: Response) => {
+            try {
+                const cameraParam = this.validRequest(req.body);
+                const camera = this.addCamera(cameraParam);
+                res.status(200).send(camera);
+            } catch (err) {
+                if (err instanceof InvalidTitleValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                if (err instanceof InvalidExternalValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                if (err instanceof InvalidPlanValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                res.sendStatus(500);
+            }
+        });
+
+        this.app.delete('/cameras/:cameraId', (req: Request, res: Response) => {
+            const cameraId = req.params.cameraId;
+            if (cameraId in cameras) {
+                delete cameras[cameraId];
+                res.sendStatus(200);
+            }
+            res.sendStatus(404);
+        });
+
+        this.app.put('/cameras/:cameraId', (req: Request, res: Response) => {
+            try {
+                const cameraId = req.params.cameraId;
+                if (!(cameraId in cameras)) {
+                    res.sendStatus(404);
+                }
+                const cameraParam = this.validRequest(req.body);
+
+                const camera = cameras[cameraId];
+                camera.title = cameraParam.title;
+                camera.plan = cameraParam.plan;
+                camera.external = cameraParam.external;
+
+                res.status(200).send(camera);
+            } catch (err) {
+                if (err instanceof InvalidTitleValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                if (err instanceof InvalidExternalValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                if (err instanceof InvalidPlanValue) {
+                    return res.status(400).send({ 'errMessage': err.message });
+                }
+                res.sendStatus(500);
+            }
+        });
+
+        this.app.get('/placeholder', (req: Request, res: Response) => {
+            res.sendFile('placeholder.jpg', { root: __dirname });
+        });
+    }
+
+    async initialize(): Promise<void> {
+        console.log("[ReactTest] - Initializing express Web service");
+
+        return new Promise((resolve) => {
+            this.app.listen(this.port, () => {
+                console.log("[ReactTest] - Web service listening on port", this.port);
+                resolve();
+            });
+        });
+    }
+
+}
+
+const webService = new WebService();
+
+webService.addCamera({
+    title: 'Câmera Rua Lobos',
+    external: true,
+    plan: '1dia'
+});
+
+webService.initialize();
